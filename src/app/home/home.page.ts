f6ae35479e9b71e9d3ffcc96282df45acab36629
Yatin Patel <yatinweb@gmail.com>
2024-08-09 19:42:07 +0530

    Added Leage Page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,43 @@
+import { NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonListHeader, IonLabel, IonItem, IonThumbnail, IonRadioGroup, IonRadio, IonFooter, IonButtons, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonButtons, IonFooter, IonRadio, IonRadioGroup, IonItem, IonLabel, IonListHeader, IonList, IonHeader, IonToolbar, IonTitle, IonContent, IonThumbnail, NgForOf],
 })
 export class HomePage {
+  leagueList = [
+    {
+      id: 1,
+      name: 'EPL',
+      gameType: 'football',
+      logoUrl: 'https://picsum.photos/100/100/?blur=2&random=1',
+    },
+    {
+      id: 2,
+      name: 'NBL',
+      gameType: 'football',
+      logoUrl: 'https://picsum.photos/100/100/?blur=2&random=2',
+    },
+    {
+      id: 3,
+      name: 'BBL',
+      gameType: 'football',
+      logoUrl: 'https://picsum.photos/100/100/?blur=2&random=3',
+    },
+  ];
+
+  selectedLeague: any;
+
   constructor() {}
+
+  handleChange(ev: any) {
+    this.selectedLeague = ev.target.value;
+    console.log(this.selectedLeague);
+  }
+
 }
